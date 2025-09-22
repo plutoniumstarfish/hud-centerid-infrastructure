541da973119d9ff39f8b7e36f60d2ede4b380cf9
Genesis Ancheta <plutoniumstarfish@yahoo.com>
2025-09-23 04:58:47 +0800

    update network

---
+++ infra-vpc/network/locals.tf
@@ -0,0 +1,4 @@
+locals {
+    zone1 = "us-east-1a"
+    zone2 = "us-east-1b"
+}
--- infra-vpc/network/main.tf
+++ infra-vpc/network/main.tf
@@ -5,11 +5,42 @@ locals {
     account_id = data.aws_caller_identity.current.account_id
 }
 
+#vpc
 resouce "aws_vpc" "main_vpc" {
     cidr_block = var.vpc_cidr_block
+    enable_dns_support = true
+    enable_dns_hostname = true
     tags = {
-        Name = "${var.project_name}-vpc"
-        Environment = "${var.environment}"
-        CreatedBy = "Terraform"
+        "Name" = "${var.project_name}-vpc"
+        "Environment" = "${var.environment}"
+        "CreatedBy" = "Terraform"
+    }
+}
+
+#internet gateway
+resource "aws_internet_gateway" "main_igw" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    tags = {
+        "Name" = "${var.project_name}-igw"
+        "Environment" = "${var.environment}"
+        "CreatedBy" = "Terraform"
+    }
+}
+
+#subnets
+resource "aws_subnet" "private_zone1" {
+    vpc_id       = aws_vpc.main_vpc.id
+    cidr_block   = "10.0.0.0/19"
+    availability_zone = local.zone1
+
+    tags = {
+        "Name"                                                 = "${var.environment}-private-${local.zone1}"
+        "Environment" = "${var.environment}"
+        "kubernetes.io/role/internal-elb"                      = "1"
+        "kubernetes.io/cluster/${var.env}-${var.project_name}" = "${var.project_name}"
+
+
+
     }
 }
